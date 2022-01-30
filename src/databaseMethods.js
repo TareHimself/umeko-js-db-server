5b315616e900e2159aa01cd8f6cc289a697449c2
TareGit <Tareebelo4@gmail.com>
2022-01-29 23:06:22 -0500

    db rewrite

---
+++ src/databaseMethods.js
@@ -0,0 +1,398 @@
+
+const cluster = require('cluster');
+const axios = require('axios');
+const fs = require('fs');
+const db = require('better-sqlite3')('./src/database.db', { fileMustExist: true });
+
+function time(sep = '') {
+
+    const currentDate = new Date();
+
+    if (sep === '') {
+        return currentDate.toUTCString();
+    }
+
+    const date = ("0" + currentDate.getUTCDate()).slice(-2);
+
+    const month = ("0" + (currentDate.getUTCMonth() + 1)).slice(-2);
+
+    const year = currentDate.getUTCFullYear();
+
+    const hours = ("0" + (currentDate.getUTCHours())).slice(-2);
+
+    const minutes = ("0" + (currentDate.getUTCMinutes())).slice(-2);
+
+    const seconds = ("0" + currentDate.getUTCSeconds()).slice(-2);
+
+    return `${year}${sep}${month}${sep}${date}${sep}${hours}${sep}${minutes}${sep}${seconds}`;
+}
+
+function log(data) {
+
+    const argumentValues = Object.values(arguments);
+
+    const stack = new Error().stack;
+    const pathDelimiter = process.platform === 'linux' ? '/' : '\\';
+    const simplifiedStack = stack.split('\n')[2].split(pathDelimiter);
+    const file = simplifiedStack[simplifiedStack.length - 1].split(')')[0];
+    argumentValues.unshift(`${file} ::`);
+
+    argumentValues.unshift(`${cluster.isMaster ? "Master" : "Child"} ID-${process.pid} ::`);
+
+    argumentValues.unshift(`${time(':')} ::`);
+
+
+
+    console.log.apply(null, argumentValues);
+}
+
+function backup(db, logData) {
+
+    db.backup(`./src/backups/backup-${time('-')}.db`)
+        .then(() => {
+            log(`Backup Complete.`);
+        })
+        .catch((error) => {
+            log(`Backup Failed.`, error);
+        });
+}
+
+function verifyRequest(request) {
+    if (request.get('x-api-key') === undefined) return false;
+
+    if (request.get('x-api-key') !== process.env.UMEKO_TOKEN) return false;
+
+    return true;
+}
+
+function doesTableExist(tableName) {
+
+    const statement = `SELECT name FROM sqlite_master WHERE type='table' AND name='${tableName}'`;
+
+    const tables = db.prepare(statement).all();
+
+    return tables.length > 0;
+}
+
+async function getServerInfo(request, response) {
+    try {
+        response.send({ id: 'umeko-js-db-server' });
+    } catch (error) {
+        log(error);
+    }
+}
+
+async function getServerPing(request, response) {
+    try {
+        response.send({ recieved_at: Date.now() });
+    } catch (error) {
+        log(error);
+    }
+}
+
+async function getTables(request, response) {
+    try {
+
+        let result = {};
+
+        try {
+
+            let whereStatement = '';
+
+            const specificRows = request.body;
+
+            if(specificRows && specificRows.length)
+            {
+                whereStatement += 'AND (';
+
+                specificRows.forEach(row => {
+                    whereStatement += `name='${row}'${row === specificRows[specificRows.length - 1] ? ")" : " OR "}`;
+                })
+            }
+
+            const fields = request.query.fields !== undefined ? request.query.fields : '*';
+            const sqliteStatement = `SELECT ${fields} FROM sqlite_master WHERE type='table' ${whereStatement} ;`;
+
+            log(sqliteStatement);
+            
+            result = db.prepare(sqliteStatement).all();
+            response.send(result);
+        } catch (error) {
+            response.send({error : error.message});
+        }
+
+        log('Get Table Request Processed')
+        
+    } catch (error) {
+        log(error);
+    }
+    
+}
+
+async function getRows(request, response) {
+    try {
+        let results = [];
+
+        const tableName = request.params.tableId;
+
+        if (!doesTableExist(tableName)) return response.send({ error: "Table does not exist" });
+
+        try {
+            const fields = request.query.fields !== undefined ? request.query.fields : '*';
+
+            let whereStatement = '';
+
+            const specificRows = request.body;
+
+            if(specificRows && specificRows.length)
+            {
+                whereStatement += 'WHERE ';
+
+                const primaryKey = db.prepare(`Select name FROM pragma_table_info('${tableName}') WHERE pk=1;`).pluck().all()[0];
+                specificRows.forEach(row => {
+                    whereStatement += `${primaryKey}='${row}'${row === specificRows[specificRows.length - 1] ? "" : " OR "}`;
+                })
+            }
+            const sqliteStatement = `SELECT ${fields} FROM '${tableName}' ${whereStatement}`;
+
+
+            results = db.prepare(sqliteStatement).all();
+            response.send(results);
+        } catch (error) {
+            response.send({error : error.message});
+        }
+
+        log('Get rows Request Processed')
+        
+    } catch (error) {
+        log(error);
+    }
+}
+
+async function createTables(request, response) {
+    try {
+        const body = request.body;
+
+        if (!body.length) return response.send({ error: 'no tables sent' });
+
+        const results = [];
+
+        body.forEach((table) => {
+            let sqliteStatement = `CREATE TABLE IF NOT EXISTS ${table.name} (\n`;
+
+            table.rows.forEach(element => {
+                sqliteStatement += `${element.name} ${element.type} ${element.option} ${element === table.rows[table.rows.length - 1] ? "" : ","}\n`
+            });
+
+            sqliteStatement += ');';
+
+            try {
+                const statementExecution = db.prepare(sqliteStatement).run();
+
+                results.push({ result: 'success' });
+            } catch (error) {
+                log(error)
+                results.push({ error: error })
+            }
+
+
+        })
+
+        response.send(results);
+
+    } catch (error) {
+        log(error);
+    }
+}
+
+async function updateColumns(request, response) {
+    try {
+        if (!doesTableExist(request.params.tableId)) return response.send({ error: "Table does not exist" });
+
+        const body = request.body;
+
+        response.send({ 'result': 'Not yet implemented' });
+    } catch (error) {
+        log(error);
+    }
+}
+
+async function updateRows(request, response) {
+    try {
+        const tableName = request.params.tableId;
+
+        if (!doesTableExist(tableName)) return response.send({ error: "Table does not exist" });
+
+        const rowsFromUser = request.body;
+
+        if (!rowsFromUser || !rowsFromUser.length) return response.send({ error: "No rows sent" })
+
+
+
+        const primaryKey = db.prepare(`Select name FROM pragma_table_info('${tableName}') WHERE pk=1;`).pluck().all()[0];
+
+        let whereStatement = 'WHERE ';
+
+        rowsFromUser.forEach(function (row) {
+            whereStatement += `${primaryKey}='${row[primaryKey]}'${row === rowsFromUser[rowsFromUser.length - 1] ? "" : " OR "}`;
+        })
+
+        
+
+        const rowsInTable = db.prepare(`SELECT ${primaryKey} FROM '${tableName}' ${whereStatement}`).all();
+
+        const primaryKeysInTable = rowsInTable.map(row => row[primaryKey]);
+
+        const rowsToInsert = rowsFromUser.filter(row => !primaryKeysInTable.includes(row[primaryKey]));
+        const rowsToUpdate = rowsFromUser.filter(row => primaryKeysInTable.includes(row[primaryKey]));
+
+        const result = { inserted : 0 , updated : 0};
+
+        rowsToInsert.forEach((row)=>{
+
+                const keys = Object.keys(row);
+                const values = Array.from(keys);
+
+                values.forEach(function (value, i) {
+                    values[i] = "@" + value;
+                });
+
+                const finalStatement = `INSERT INTO \`${tableName}\` (${keys.toString()}) VALUES (${values.toString()})`;
+
+                const result = db.prepare(finalStatement).run(row);
+
+                if (result.changes > 0) {
+                    result.inserted++;
+                }
+        });
+
+        rowsToUpdate.forEach((row)=>{
+            const keys = Object.keys(row);
+
+                keys.forEach(function (key, i) {
+                    if (typeof row[key] !== 'number') {
+                        keys[i] = `${key}='${row[key]}'`;
+                    }
+                    else {
+                        keys[i] = `${key}=${row[key]}`;
+                    }
+
+                });
+
+                const finalStatement = `UPDATE \`${tableName}\` SET ${keys.toString()} WHERE ${primaryKey}='${row[primaryKey]}'`;
+
+                const result = db.prepare(finalStatement).run(row);
+
+                if (result.changes > 0) {
+                    result.updated++;
+                }
+        });
+
+        response.send(result);
+
+    } catch (error) {
+        log(error);
+        response.send({ 'error': error.message });
+    }
+}
+
+async function deleteTables(request, response) {
+    try {
+
+        const tablesFromUser = request.params.tableId;
+
+        if (!tablesFromUser || !tablesFromUser.sort) response.send({ error: "Please send an empty array to delete all tables" });
+
+        if (tablesFromUser.length) {
+
+            tablesFromUser.forEach((table) => {
+                const sqliteStatement = `DROP TABLE IF EXISTS \`${table}\`;`;
+                db.prepare(sqliteStatement).run();
+
+            });
+
+            response.send({ result: 'success' });
+        }
+        else {
+            let tables = []
+
+            tables = db.prepare(`select name from sqlite_master where type='table';`).all();
+
+            tables.forEach(function (table, i) {
+                tables[i] = table.name;
+            });
+
+            tables.forEach(function (table) {
+                const sqliteStatement = `DROP TABLE IF EXISTS \`${table}\`;`;
+                db.prepare(sqliteStatement).run();
+            })
+
+            response.send({ result: 'success' });
+        }
+
+    } catch (error) {
+        log(error);
+    }
+}
+
+async function deleteRows(request, response) {
+    try {
+        const tableName = request.params.tableId;
+
+        if (!doesTableExist( tableName)) return response.send({ error: "Table does not exist" });
+
+        const body = request.body;
+
+        try {
+            const keys = Object.keys(body);
+
+            keys.forEach(function (key, i) {
+                if (typeof body[key] !== 'number') {
+                    keys[i] = `${key}='${body[key]}'`;
+                }
+                else {
+                    keys[i] = `${key}=${body[key]}`;
+                }
+            });
+
+            const whereStatement = request.query.where !== undefined ? `WHERE ${request.query.where}` : '';
+            if (whereStatement === '') {
+                response.status(200)
+                response.send({ error: `No 'Where'statement specified` });
+                return;
+            }
+
+            const result = db.prepare(`DELETE FROM \`${tableName}\` ${whereStatement};`).run();
+
+            response.status(200);
+
+            if (result.changes > 0) {
+                response.send({ 'result': 'success' });
+            }
+            else {
+                response.send({ 'error': 'The row could not be deleted or does not exist' });
+            }
+
+        } catch (error) {
+            log(error)
+            return response.send({ 'error': error.message });
+        }
+    } catch (error) {
+        log(error);
+    }
+}
+
+module.exports = {
+
+    log: log,
+    verifyRequest : verifyRequest,
+    getServerInfo: getServerInfo,
+    getServerPing: getServerPing,
+    getTables: getTables,
+    getRows: getRows,
+    createTables: createTables,
+    updateColumns: updateColumns,
+    updateRows: updateRows,
+    deleteTables: deleteTables,
+    deleteRows: deleteRows
+}
--- src/dbProcess.js
+++ src/dbProcess.js
@@ -1,10 +1,7 @@
-const cluster = require('cluster');
-const fs = require('fs');
 const cors = require('cors');
 const express = require('express');
 const http = require('http');
-const axios = require('axios');
-const db = require('better-sqlite3')('./src/database.db', { fileMustExist: true });
+const methods = require('./databaseMethods');
 
 const app = express();
 
@@ -15,381 +12,61 @@ app.set('trust proxy', true)
 
 const port = process.argv.includes('debug') ? 49153 : 8080;
 
-
-function time(sep = '') {
-
-    const currentDate = new Date();
-
-    if (sep === '') {
-        return currentDate.toUTCString();
-    }
-
-    const date = ("0" + currentDate.getUTCDate()).slice(-2);
-
-    const month = ("0" + (currentDate.getUTCMonth() + 1)).slice(-2);
-
-    const year = currentDate.getUTCFullYear();
-
-    const hours = ("0" + (currentDate.getUTCHours())).slice(-2);
-
-    const minutes = ("0" + (currentDate.getUTCMinutes())).slice(-2);
-
-    const seconds = ("0" + currentDate.getUTCSeconds()).slice(-2);
-
-    return `${year}${sep}${month}${sep}${date}${sep}${hours}${sep}${minutes}${sep}${seconds}`;
-}
-
-function log(data) {
-
-    const argumentValues = Object.values(arguments);
-
-    const stack = new Error().stack;
-    const pathDelimiter = process.platform === 'linux' ? '/' : '\\';
-    const simplifiedStack = stack.split('\n')[2].split(pathDelimiter);
-    const file = simplifiedStack[simplifiedStack.length - 1].split(')')[0];
-    argumentValues.unshift(`${file} ::`);
-
-    argumentValues.unshift(`${cluster.isMaster ? "Master" : "Child"} ID-${process.pid} ::`);
-
-    argumentValues.unshift(`${time(':')} ::`);
-
-    
-
-    console.log.apply(null, argumentValues);
-}
-
-function backup(db, logData) {
-
-    db.backup(`./src/backups/backup-${time('-')}.db`)
-        .then(() => {
-            log(`Backup Complete.`);
-        })
-        .catch((error) => {
-            log(`Backup Failed.`, error);
-        });
-}
-
-function verifyRequest(request) {
-    if (request.get('x-umeko-token') === undefined) return false;
-
-    if (request.get('x-umeko-token') !== process.env.UMEKO_TOKEN) return false;
-
-    return true;
-}
-
-function doesTableExist(tableName) {
-
-    const statement = `SELECT name FROM sqlite_master WHERE type='table' AND name='${tableName}'`;
-
-    const tables = db.prepare(statement).all();
-
-    return tables.length > 0;
-}
-
 app.get('/', async (request, response) => {
-    response.send({id : 'umeko-js-db-server'});
+    methods.getServerInfo(request,response).catch(methods.log);
 });
 
 app.get('/ping', async (request, response) => {
-    response.send({ recieved_at : Date.now() });
+    methods.getServerPing(request,response).catch(methods.log);
 });
 
 app.use(function (request, response, next) {
-    if (!verifyRequest(request)) {
+    if (!methods.verifyRequest(request)) {
         response.status(401);
-        log('Recieved Unauthorized Request')
+        methods.log('Recieved Unauthorized Request')
         return response.send({ error: "Invalid Authentication" });
     }
     next();
 });
 
-// Add a new table to the database
-app.post('/tables', async (request, response) => {
-    try {
-
-        const body = request.body;
-
-        let sqliteStatement = `CREATE TABLE IF NOT EXISTS ${body.name} (\n`;
-
-        body.rows.forEach(element => {
-            sqliteStatement += `${element.name} ${element.type} ${element.option} ${element === body.rows[body.rows.length - 1] ? "" : ","}\n`
-        });
-
-        sqliteStatement += ');';
-
-        try {
-            const statementExecution = db.prepare(sqliteStatement).run();
-            response.status(200)
-            response.send({ result: 'success' });
-        } catch (error) {
-            log(error)
-            response.status(200)
-            response.send({ error: error });
-        }
-
-    } catch (error) {
-        log(error);
-    }
-});
-
-// add a column to the specified tabe
-app.post('/tables/:tableId', async (request, response) => {
-    try {
-
-
-        response.status(200)
-
-        if (!doesTableExist(request.params.tableId)) return response.send({ error: "Table does not exist" });
-
-        const body = request.body;
-
-        response.send({ 'result': 'Not yet implemented' });
-    } catch (error) {
-        log(error);
-    }
-});
-
-// add/update update row to/in the specified table
-app.post('/tables/:tableId/rows', (request, response) => {
-    try {
-        const tableName = request.params.tableId;
-
-        if (!doesTableExist( tableName)) return response.send({ error: "Table does not exist" });
-
-        const body = request.body;
-
-        try {
-
-            const primaryKey = db.prepare(`Select name FROM pragma_table_info('${tableName}') WHERE pk=1;`).pluck().all();
-            const checkStatement = `SELECT ${primaryKey[0]} FROM ${tableName} WHERE ${primaryKey[0]}='${body[primaryKey[0]]}'`;
-            const isInsert = db.prepare(checkStatement).all().length === 0;
-
-            if (isInsert) {
-                const keys = Object.keys(body);
-                const values = Array.from(keys);
-
-                values.forEach(function (value, i) {
-                    values[i] = "@" + value;
-                });
-
-                const finalStatement = `INSERT INTO \`${tableName}\` (${keys.toString()}) VALUES (${values.toString()})`;
-
-                const result = db.prepare(finalStatement).run(body);
-
-                if (result.changes > 0) {
-                    response.send({ 'result': 'success' });
-                }
-                else {
-                    response.send({ 'error': 'The row could not be inserted' });
-                }
-            }
-            else {
-                const keys = Object.keys(body);
-
-                keys.forEach(function (key, i) {
-                    if (typeof body[key] !== 'number') {
-                        keys[i] = `${key}='${body[key]}'`;
-                    }
-                    else {
-                        keys[i] = `${key}=${body[key]}`;
-                    }
-
-                });
-
-                const finalStatement = `UPDATE \`${tableName}\` SET ${keys.toString()} WHERE ${primaryKey[0]}='${body[primaryKey[0]]}'`;
-
-                const result = db.prepare(finalStatement).run(body);
-
-                if (result.changes > 0) {
-                    response.send({ 'result': 'success' });
-                }
-                else {
-                    response.send({ 'error': 'The row could not be updated' });
-                }
-
-            }
-
-        } catch (error) {
-            log(error)
-            return response.send({ 'error': error.message });
-        }
-
-    } catch (error) {
-        log(error);
-    }
-});
-
 // get all the tables in the database
 app.get('/tables', (request, response) => {
-    try {
-        let results = [];
-
-        const tableName = request.params.tableId;
-
-        if (tableName && !doesTableExist( tableName)) return response.send({ error: "Table does not exist" });
-
-        try {
-            results = db.prepare(`select name from sqlite_master where type='table';`).all();
-
-            results.forEach(function (result, i) {
-                results[i] = result.name;
-            });
-        } catch (error) {
-            log(error);
-        }
-
-        log('Get Tables Request Processed')
-        response.status(200)
-        response.send({ data: results });
-    } catch (error) {
-        log(error);
-    }
-});
-
-// get all the tables in the database
-app.get('/tables/:tableId', (request, response) => {
-    try {
-        const tableName = request.params.tableId;
-
-        if (!doesTableExist( tableName)) return response.send({ error: "Table does not exist" });
-
-        let result = {};
-
-        try {
-            const fields = request.query.fields !== undefined ? request.query.fields : '*';
-            const sqliteStatement = `SELECT ${fields} FROM sqlite_master where type='table' AND name='${request.params.tableId}';`;
-            result = db.prepare(sqliteStatement).all();
-        } catch (error) {
-            log(error);
-        }
-
-        log('Get Table Request Processed')
-        response.send(result);
-    } catch (error) {
-        log(error);
-    }
+    methods.getTables(request,response);
 });
 
 // get a row or all the rows from a table
 app.get('/tables/:tableId/rows', (request, response) => {
-    try {
-        let results = [];
-
-        const tableName = request.params.tableId;
-
-        if (!doesTableExist( tableName)) return response.send({ error: "Table does not exist" });
-
-        try {
-            const fields = request.query.fields !== undefined ? request.query.fields : '*';
-            const whereStatement = request.query.where !== undefined ? `WHERE ${request.query.where}` : '';
-            const sqliteStatement = `SELECT ${fields} FROM '${request.params.tableId}' ${whereStatement}`;
-            results = db.prepare(sqliteStatement).all();
-        } catch (error) {
-            log(error);
-        }
-
-        log('Get rows Request Processed')
-        response.status(200)
-        response.send({ data: results });
-    } catch (error) {
-        log(error);
-    }
+    methods.getRows(request,response);
 });
 
+// Add a new tables to the database
+app.post('/tables', async (request, response) => {
+    methods.createTables(request,response);
+});
 
+// add a column to the specified tabe
+app.post('/tables/:tableId', async (request, response) => {
+    methods.updateColumns(request,response);
+});
 
-// delete a table
-app.delete('/tables/:tableId', (request, response) => {
-    try {
-        const tableName = request.params.tableId;
-
-        if (!doesTableExist( tableName)) return response.send({ error: "Table does not exist" });
-
-        const fields = request.query.fields !== undefined ? request.query.fields : '*';
-        const whereStatement = request.query.where !== undefined ? `WHERE ${request.query.where}` : '';
-        const sqliteStatement = `DROP TABLE IF EXISTS \`${tableName}\`;`;
-        db.prepare(sqliteStatement).run();
-        response.status(200)
-        response.send({ result: 'success' });
-    } catch (error) {
-        log(error);
-    }
+// add/update update row to/in the specified table
+app.post('/tables/:tableId/rows', (request, response) => {
+    methods.updateRows(request,response);
 });
 
 // delete all the tables
 app.delete('/tables', (request, response) => {
-    try {
-        let tables = []
-
-        tables = db.prepare(`select name from sqlite_master where type='table';`).all();
-
-        tables.forEach(function (table, i) {
-            tables[i] = table.name;
-        });
-
-        tables.forEach(function (table) {
-            const sqliteStatement = `DROP TABLE IF EXISTS \`${table}\`;`;
-            db.prepare(sqliteStatement).run();
-        })
-
-        response.status(200)
-        response.send({ result: 'success' });
-    } catch (error) {
-        log(error);
-    }
+    methods.deleteTables(request,response);
 });
 
 // delete a row in a table
 app.delete('/tables/:tableId/rows', (request, response) => {
-    try {
-        const tableName = request.params.tableId;
-
-        if (!doesTableExist( tableName)) return response.send({ error: "Table does not exist" });
-
-        const body = request.body;
-
-        try {
-            const keys = Object.keys(body);
-
-            keys.forEach(function (key, i) {
-                if (typeof body[key] !== 'number') {
-                    keys[i] = `${key}='${body[key]}'`;
-                }
-                else {
-                    keys[i] = `${key}=${body[key]}`;
-                }
-            });
-
-            const whereStatement = request.query.where !== undefined ? `WHERE ${request.query.where}` : '';
-            if (whereStatement === '') {
-                response.status(200)
-                response.send({ error: `No 'Where'statement specified` });
-                return;
-            }
-
-            const result = db.prepare(`DELETE FROM \`${tableName}\` ${whereStatement};`).run();
-
-            response.status(200);
-
-            if (result.changes > 0) {
-                response.send({ 'result': 'success' });
-            }
-            else {
-                response.send({ 'error': 'The row could not be deleted or does not exist' });
-            }
-
-        } catch (error) {
-            log(error)
-            return response.send({ 'error': error.message });
-        }
-    } catch (error) {
-        log(error);
-    }
+    methods.deleteRows(request,response);
 });
 
 
 
 app.listen(port, () => {
-    log(`Database HTTP Server listening at http://localhost:${port}/`)
+    methods.log(`Database HTTP Server listening at http://localhost:${port}/`)
 });
 
